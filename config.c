13320d23c773849a9375c145686ce138d331b5d5
alti-tude <kartik.gupta0204@gmail.com>
2019-08-21 22:03:19 +0530

    bug psudo fixed

--- config.c
+++ config.c
@@ -1,7 +1,7 @@
 #include<unistd.h>
 
 long long int PATH_SIZE = 1000; 
-size_t BUF_SIZE = 131072;
+size_t BUF_SIZE = 200000;
 char* delims=" ;\t\n\r";
 long long int NUM_CMDS = 10;
 long long int MAX_PROC = 10;
